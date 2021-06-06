aea6c8899390af744a6404fb6012451e62791502
Takeru Ohta <phjgt308@gmail.com>
2021-06-06 10:48:09 +0900

    Remove tokio runtime

--- src/main.rs
+++ src/main.rs
@@ -8,30 +8,13 @@ enum Opt {
     Show(weih::cli::show::ShowOpt),
 }
 
-fn main() -> anyhow::Result<()> {
+#[actix_web::main]
+async fn main() -> anyhow::Result<()> {
     let opt = Opt::from_args();
     match opt {
-        Opt::Get(_) | Opt::Show(_) => tokio_main(opt)?,
-        Opt::Run(_) => actix_web_main(opt)?,
-    }
-    Ok(())
-}
-
-#[tokio::main]
-async fn tokio_main(opt: Opt) -> anyhow::Result<()> {
-    match opt {
         Opt::Get(o) => o.execute().await?,
-        Opt::Show(o) => o.execute().await?,
-        _ => unreachable!(),
-    }
-    Ok(())
-}
-
-#[actix_web::main]
-async fn actix_web_main(opt: Opt) -> anyhow::Result<()> {
-    match opt {
         Opt::Run(o) => o.execute().await?,
-        _ => unreachable!(),
+        Opt::Show(o) => o.execute().await?,
     }
     Ok(())
 }
